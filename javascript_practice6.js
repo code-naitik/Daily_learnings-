1db3fcf15ef1b609bace2638259d11dc23b8d496
code-naitik <post.naitik@gmail.com>
2025-07-04 15:42:46 +0530

    javascript_practice6.js

---
+++ javascript_practice6.js
@@ -0,0 +1,21 @@
+//1. Get First Character of a String//
+console.log(firstChar("JavaScript")); 
+// Output: J
+
+console.log(firstChar("")); 
+// Output: ""
+
+
+
+
+//2. Repeat a String
+console.log(repeatString("Hi", 3)); 
+// Output: HiHiHi
+
+
+//3.Check if Number is Divisible by 5
+console.log(isDivisibleBy5(10)); 
+// Output: true
+
+console.log(isDivisibleBy5(7)); 
+// Output: false
