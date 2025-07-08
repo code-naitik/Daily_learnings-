f7706c3c4397131a17882897600ba5b8224a9e47
code-naitik <post.naitik@gmail.com>
2025-07-08 16:40:36 +0530

    JavaScript_practice9.js

---
+++ JavaScript_practice9.js
@@ -0,0 +1,13 @@
+//1. Sum of Positive Numbers Only
+console.log(sumPositive([-1, 3, 5, -2, 7])); // Output: 15
+
+
+
+//2.Count Occurrences of a Character
+console.log(countChar("banana", "a")); // Output: 3
+
+
+
+//3.Check if All Elements are Unique
+console.log(allUnique([1, 2, 3, 4])); // Output: true
+console.log(allUnique([1, 2, 2, 3])); // Output: false
