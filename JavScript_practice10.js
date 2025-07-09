67b98f696337fec5d5d33615bc07447bf1de9287
code-naitik <post.naitik@gmail.com>
2025-07-09 17:19:03 +0530

    JavScript_practice10.js

---
+++ JavScript_practice10.js
@@ -0,0 +1,18 @@
+//1. Check If a String Contains a Substring
+console.log(containsSubstring("I love JavaScript", "love"));
+// Output: true
+
+
+
+//2.Sum All Even Numbers in an Array
+console.log(sumEven([1, 2, 3, 4, 5, 6])); 
+// Output: 12
+
+
+
+//3.Check If Array is Sorted
+console.log(isSorted([1, 2, 3, 4])); 
+// Output: true
+
+console.log(isSorted([1, 3, 2]));   
+// Output: false
