398b99497381adb13b0ca29b03d46825aa0048d9
code-naitik <post.naitik@gmail.com>
2025-07-10 17:02:55 +0530

    javascript_practice11.js

---
+++ javascript_practice11.js
@@ -0,0 +1,14 @@
+//1. Subtract Numbers
+let difference = 9 - 4;
+console.log(difference);
+
+
+//2. Check if String Contains “a”
+let word = "cat";
+console.log(word.includes("a"));
+
+
+//3. Convert Minutes to Seconds
+let minutes = 3;
+let seconds = minutes * 60;
+console.log(seconds);
